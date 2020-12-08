9c7f2b5f5499e322708056b12716345a698356c6
xingliudiexue <2658756105@qq.com>
2020-12-08 14:39:55 +0800

    Site updated: 2020-12-08 14:39:52

--- js/self.js
+++ js/self.js
@@ -382,3 +382,14 @@ if (full_page.length != 0) {
   full_page[0].style.background = "transparent";
 }*/
 
+$(document).ready(function() {
+    var int = setInterval(fixCount, 50);  // 50ms周期检测函数
+    var countOffset = 50;  // 初始化首次数据
+   function fixCount() {                   
+     if ($("#busuanzi_container_site_pv").css("display") != "none")
+       {
+          $("#busuanzi_value_site_pv").html(parseInt($("#busuanzi_value_site_pv").html()) + countOffset); // 加上初始数据 
+          clearInterval(int); // 停止检测
+     }  
+ }           
+});
